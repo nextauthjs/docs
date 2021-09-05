22f79bd557397d48c6b59dd6caca64eaa9b806a2
ndom91 <yo@ndo.dev>
2021-09-05 22:14:17 +0200

    fix: homepage provider example

--- src/pages/index.js
+++ src/pages/index.js
@@ -258,25 +258,28 @@ export default function Component() {
 
 const serverlessFunctionCode = `
 import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import AppleProvider from 'next-auth/providers/apple'
+import FacebookProvider from 'next-auth/providers/facebook'
+import GoogleProvider from 'next-auth/providers/google'
+import EmailProvider from 'next-auth/providers/email'
 
 export default NextAuth({
   providers: [
     // OAuth authentication providers...
-    Providers.Apple({
+    AppleProvider({
       clientId: process.env.APPLE_ID,
       clientSecret: process.env.APPLE_SECRET
     }),
-    Providers.Facebook({
+    FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET
     }),
-    Providers.Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
     // Passwordless / email sign in
-    Providers.Email({
+    EmailProvider({
       server: process.env.MAIL_SERVER,
       from: 'NextAuth.js <no-reply@example.com>'
     }),
