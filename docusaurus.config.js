e46e50303d952e96abd6c29c79ef417fa4bb9f71
Nico Domino <yo@ndo.dev>
2021-09-20 12:30:33 +0200

    fix: update announcement bar wording (#34)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,7 +66,7 @@ module.exports = {
     announcementBar: {
       id: "new-major-announcement",
       content:
-        "The default documentation is for v4 , which is currently in beta ⚠. If you need the old (v3) documentation, check it out <a href='/v3/getting-started/introduction'>here</a> (make sure the url contains /v3/). Migration docs to v4 <a href='/getting-started/upgrade-v4'>can be found here</a>.",
+        "The default documentation is for v4, which is currently in beta 🚨 migration to <b>v4</b> docs can be found <a href='/getting-started/upgrade-v4'>here</a> 👈 The old v3 docs can be found <a href='/v3/getting-started/introduction'>here</a>.",
       backgroundColor: "#1786fb",
       textColor: "#fff",
     },
